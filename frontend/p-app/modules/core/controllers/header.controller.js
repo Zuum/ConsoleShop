6da094bc9de46fa6d87828ae3041d3e56df3880c
Илья Полищук <djzuumik@gmail.com>
2017-04-04 15:36:40 +0300

    htmls cleared

--- frontend/p-app/modules/core/controllers/header.controller.js
+++ frontend/p-app/modules/core/controllers/header.controller.js
@@ -4,9 +4,4 @@ angular
   .module('core')
   .controller('HeaderController', ['$scope', '$state', ($scope, $state) => {
     let vm = $scope;
-
-    vm.links = window.configData.headerItems;
-    vm.active = (link) => {
-      return $state.includes(link.url.replace('.main', '')) ? 'active' : '';
-    };
   }]);
