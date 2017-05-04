b54449964ccac0aeedd1186d04a5a9d60f020b5a
Илья Полищук <djzuumik@gmail.com>
2017-05-04 12:39:01 +0300

    goods module start

--- migrations/1493728783960-add-clocks.js
+++ migrations/1493728783960-add-clocks.js
@@ -5,12 +5,30 @@ const Goods = require('../server/configs/sequelize.js').models.Goods;
 const GoodsCategories = require('../server/configs/sequelize.js').models.GoodsCategories;
 const clocksData = require('../server/configs/db.js').postgres.initialData.clocks;
 const Promise = require('bluebird');
-const copy = require('../server/utils/clone-machine.js');
 
 exports.up = (next) => {
-
+  return Promise.map(clocksData, (clock) => {
+    return GoodsCategories.findOne({ where: { code: clock.categoryCode } })
+    .then((category) => {
+          delete clock.categotyName;
+          clock.categoryId = category.id;
+          return Goods.create(clock);
+        })
+  })
+  .nodeify(next)
 };
 
 exports.down = (next) => {
-
+  var codes = [];
+  return Promise.map(clocksData, (clock) => {
+    codes.push(clock.code);
+  })
+      .then(() => {
+    return Goods.destroy({
+      where: {
+        code: { $in: codes }
+      }
+    })
+  })
+  .nodeufy(next)
 };
